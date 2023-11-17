c98e3c55edc8bc1671c12967775749eb410f5f81
Alex <aprosyanovskij1@gmail.com>
2023-11-17 16:23:48 +0200

    Initial Commit

---
+++ Candy Calis/Candy_CalisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Candy_CalisApp.swift
+//  Candy Calis
+//
+//  Created by Прос Александр on 17.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Candy_CalisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
